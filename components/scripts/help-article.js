7e78f1fdadfbf21691fe5f2afc590c62ced92c2a
Malcolm Bellman <mbellman@capstonepub.com>
2024-10-23 12:32:13 -0500

    help article updates

--- components/scripts/help-article.js
+++ components/scripts/help-article.js
@@ -25,12 +25,6 @@
 			.replace(/\[\/url\]/g, "</a>");
 	}
 
-	function formatImage(line) {
-		var path = removeDirective(line);
-
-		return `<div class="c-help-article__image"><img src=${path}></div>`;
-	}
-
 	function formatTitle(line) {
 		var title = removeDirective(line);
 
@@ -39,6 +33,24 @@
 			.replace(/[^a-z0-9_]/g, "");
 
 		return `<h2 id="${headingId}" class="c-help-article__title">${title}</h2>`;
+	}
+
+	function formatImage(line) {
+		var file = removeDirective(line);
+
+		return `<div class="c-help-article__image"><img src=${file}></div>`;
+	}
+
+	function formatVideo(line) {
+		var file = removeDirective(line);
+
+		return `
+			<button class="c-help-article__video-button" data-asset="${file}">
+				<svg width="109" height="124" viewBox="0 0 109 124" fill="none" xmlns="http://www.w3.org/2000/svg">
+					<path d="M0.46321 62.6254V107.432C0.368998 109.444 0.748793 111.45 1.57195 113.289C2.3951 115.128 3.63863 116.747 5.20235 118.018C9.7183 121.577 15.3008 123.513 21.0507 123.513C26.8006 123.513 32.383 121.577 36.899 118.018C42.6844 114.017 48.5313 110.079 54.3783 106.14C69.765 95.6765 85.1518 85.4592 100.477 74.8116C112.048 66.7489 111.248 55.6706 99.3076 47.9772C91.7373 43.0534 84.4133 37.8838 76.9661 32.8985C63.0565 23.4818 49.2084 14.0647 35.2372 4.70953C32.0635 2.62196 28.4429 1.31159 24.6683 0.883879C20.8938 0.456168 17.0714 0.923745 13.5111 2.24825C5.14075 5.14096 0.278602 10.2488 0.401696 17.8807C0.586337 32.9597 0.46321 47.7926 0.46321 62.6254Z" fill="black" fill-opacity="0.5"/>
+				</svg>
+			</button>
+		`;
 	}
 
 	function formatUnorderedListItem(line) {
@@ -63,53 +75,55 @@
 		var lines = content.split("\n");
 		var isGeneratingOrderedList = false;
 		var isGeneratingUnorderedList = false;
-		var generatedHtml = "";
+		var html = "";
 
 		lines.forEach(function(line) {
 			if (isGeneratingUnorderedList && !line.startsWith("*")) {
 				// Terminate unordered lists
-				generatedHtml += "</ul>";
+				html += "</ul>";
 				isGeneratingUnorderedList = false;
 			}
 
 			if (isGeneratingOrderedList && !/^[0-9*]./.test(line)) {
 				// Terminated ordered lists
-				generatedHtml += "</ol>";
+				html += "</ol>";
 				isGeneratingOrderedList = false;
 			}
 
 			if (line.startsWith("@image")) {
 				// Handle image directives
-				generatedHtml += formatImage(line);
+				html += formatImage(line);
 			} else if (line.startsWith("@title")) {
 				// Handle title directives
-				generatedHtml += formatTitle(line);
+				html += formatTitle(line);
+			} else if (line.startsWith("@video")) {
+				html += formatVideo(line);
 			} else if (line.startsWith("*")) {
 				// Handle unordered list items
 				if (!isGeneratingUnorderedList) {
-					generatedHtml += `<ul class="c-help-article__unordered-list">`;
+					html += `<ul class="c-help-article__unordered-list">`;
 				}
 
-				generatedHtml += formatUnorderedListItem(line);
+				html += formatUnorderedListItem(line);
 
 				isGeneratingUnorderedList = true;
 			} else if (/^[0-9*]./.test(line)) {
 				// Handle ordered list items
 				if (!isGeneratingOrderedList) {
-					generatedHtml += `<ol class="c-help-article__ordered-list">`;
+					html += `<ol class="c-help-article__ordered-list">`;
 				}
 
-				generatedHtml += formatOrderedListItem(line);
+				html += formatOrderedListItem(line);
 
 				isGeneratingOrderedList = true;
 			} else if (line.length > 0) {
-				generatedHtml += `<div class="c-help-article__step">${replaceTags(line)}</div>`;
+				html += `<div class="c-help-article__step">${replaceTags(line)}</div>`;
 			}
 
-			generatedHtml += "<br>";
+			html += "<br>";
 		});
 
-		return generatedHtml;
+		return html;
 	}
 
 	function onLinkClick(event) {
